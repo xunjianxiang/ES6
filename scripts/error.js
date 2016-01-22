d03aa7f36703df8b912836e4bd841508f9df808e
xunjianxiang <jianxiang.xun@dianjoy.com>
2016-01-22 17:27:29 +0800

    'tern-setting,eslint-setting'

--- scripts/error.js
+++ scripts/error.js
@@ -1,28 +1,28 @@
-import $location from './location'
-import $http from './http'
-import $render from './render'
-import $co from './co'
+import $location from './location';
+import $http from './http';
+import $render from './render';
+import $co from './co';
 class ErrorPage {
 	init(_route) {
-		let _this = this
+		let _this = this;
 		$co(function* () {
-			let data = yield $http.ajax(..._route.data)
-			yield $render('errorTemplate', data)
-			_this.operate()
-			return 'get in error'
+			let data = yield $http.ajax(..._route.data);
+			yield $render('errorTemplate', data);
+			_this.operate();
+			return 'get in error';
 		})
 		.then(function (data) {
-			console.log(data)
+			console.log(data);
 		})
 		.catch(function (error) {
-			console.log(error)
+			console.log(error);
 		})
 	}
 	operate() {
 		document.querySelector('button').addEventListener('click', function (event) {
-			$location.assign('/')
-			event.preventDefault()
-		})
+			$location.assign('/');
+			event.preventDefault();
+		});
 	}
 }
-export default new ErrorPage()
+export default new ErrorPage();
