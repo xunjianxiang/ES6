7d432a7b16dcb1a2a70aa19e1510903bed6cbb9c
xunjianxiang <xunjianxiang@icloud.com>
2016-01-22 19:49:47 +0800

    'delete'

--- scripts/exercise/array.js
+++
@@ -1,9 +0,0 @@
-{
-	let [a, b, c, ...z] = [1, 2, 3, 4, 5]
-	let [foo, [[bar, bar1], baz]] = [1, [[2,3], 4]]
-	console.log(a, b, c, z);
-	// console.log(foo, bar, baz);
-	console.log('\u{41}1');
-	// console.log('1'.padStart(2, '0'));
-
-}
